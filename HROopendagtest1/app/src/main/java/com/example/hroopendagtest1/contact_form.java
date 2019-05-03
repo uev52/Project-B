e0d9c967b07232f777b7329f86477061d3dcc821
YASIN078 <47326692+YASIN078@users.noreply.github.com>
2019-05-03 16:09:31 +0200

    Update contact_form.java

--- HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/contact_form.java
+++ HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/contact_form.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +14,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class contact_form extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,8 +28,6 @@ public class contact_form extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -32,7 +35,48 @@ public class contact_form extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //----------------
+
+        //In the Java programming language, the !final! keyword is used in several contexts to define
+        // an entity that can only be assigned once. Once a final variable has been assigned,
+        // it always contains the same value.
+
+        final EditText editName        = (EditText) findViewById(R.id.editName);
+        final EditText editYouremail   = (EditText) findViewById(R.id.editYouremail);
+        final EditText editSubject     = (EditText) findViewById(R.id.editSubject);
+        final EditText editContent     = (EditText) findViewById(R.id.editContent);
+
+        String Name      = editName.getText().toString();
+        String Email     = editYouremail.getText().toString();
+        String Subject   = editSubject.getText().toString();
+        String Content   = editContent.getText().toString();
+
+
+        //start maling activity, everything next is sending data from one application to another
+        Intent Mailing = new Intent(android.content.Intent.ACTION_SEND);
+
+
+        //who is the mail being sended to
+        Mailing.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"0946639@hr.nl"});
+
+        //what is the subject
+        Mailing.putExtra(android.content.Intent.EXTRA_SUBJECT, Subject);
+
+        //mail content and other information
+        Mailing.putExtra(android.content.Intent.EXTRA_TEXT, "Name:"+Name+'\n'+"Email adress:"+Email+'\n'+"Message:"+'\n'+Content);
+
+        //sharingfunctionality of the button
+        startActivity(Intent.createChooser(Mailing, "Choose mail application"));
+
+        //http://androidmkab.com/2016/12/13/create-android-contact-form-beginne/
+        //https://www.youtube.com/watch?v=tZ2YEw6SoBU
+        //-----------------
+
+
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -43,7 +87,6 @@ public class contact_form extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
-
 
 
     @SuppressWarnings("StatementWithEmptyBody")
