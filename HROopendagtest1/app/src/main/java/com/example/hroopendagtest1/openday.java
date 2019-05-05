94186bd062944a034fb7abecd245e484c2035ce5
Insecuredude <47596647+Insecuredude@users.noreply.github.com>
2019-05-05 17:49:15 +0200

    Changed some activity titles and the buttons on the General Information screen are now working properly

--- HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/openday.java
+++ HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/openday.java
@@ -2,8 +2,6 @@ package com.example.hroopendagtest1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,7 +9,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
@@ -38,7 +35,7 @@ public class openday extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         // Studyprogram button for Comunication (changes the day achedule)
-        btn_sp1=(Button)findViewById(R.id.btn_sp1);
+        btn_sp1=(Button)findViewById(R.id.contactBtn);
 
         btn_sp1.setOnClickListener(new View.OnClickListener() {
             @Override
