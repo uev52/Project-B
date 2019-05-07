6671deb0453098d7bee6028073a5e642dad425f6
YASIN078 <47326692+YASIN078@users.noreply.github.com>
2019-05-07 13:41:16 +0200

    I forgot a /, now everything works

--- HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/wijnhaven99.java
+++ HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/wijnhaven99.java
@@ -2,6 +2,7 @@ package com.example.hroopendagtest1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,44 +15,108 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ZoomControls;
 
-public class floor_plan extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    Button Wijnhaven99;
-    Button Wijnhaven103;
-    Button Wijnhaven107;
+
+public class wijnhaven99 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    ImageView Floor;
+    Button Button_GF;
+    Button Button_FF;
+    Button Button_SF;
+    Button Button_TF;;
+    TextView title;
+    ZoomControls ZoomControls;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_floor_plan);
+        setContentView(R.layout.activity_wijnhaven99);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Wijnhaven99 = (Button)findViewById(R.id.Wijnhaven99);
-        Wijnhaven99.setOnClickListener(new View.OnClickListener() {
+        title=(TextView) findViewById(R.id.FloorTitle);
+        Floor = (ImageView)findViewById(R.id.ImageView99);
+        ZoomControls = (ZoomControls)findViewById(R.id.ZoomControls);
+
+        ZoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(), wijnhaven99.class);
-                startActivity(i);
+                float x = Floor.getScaleX();
+                float y = Floor.getScaleY();
+
+                Floor.setScaleX((int) (x+1));
+                Floor.setScaleY((int) (y+1));
+
             }
         });
 
-//        Wijnhaven103 = (Button)findViewById(R.id.Wijnhaven103);
-//        Wijnhaven103.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i=new Intent(getApplicationContext(), wijnhaven103.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        Wijnhaven107 = (Button)findViewById(R.id.Wijnhaven107);
-//        Wijnhaven107.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i=new Intent(getApplicationContext(), wijnhaven107.class);
-//                startActivity(i);
-//            }
-//        });
+        ZoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float x = Floor.getScaleX();
+                float y = Floor.getScaleY();
+
+                Floor.setScaleX((int) (x-1));
+                Floor.setScaleX((int) (y-1));
+            }
+        });
+
+
+
+        Button_GF=(Button)findViewById(R.id.Button_GF);
+        Button_GF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title.setText("Ground Floor");
+                Floor.setImageResource(R.drawable.cmi900);
+
+
+            }});
+
+
+
+        Button_FF=(Button)findViewById(R.id.Button_FF);
+        Button_FF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title.setText("First Floor");
+                Floor.setImageResource(R.drawable.wh994);
+
+            }
+
+        });
+
+
+        Button_SF=(Button)findViewById(R.id.Button_SF);
+        Button_SF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title.setText("Second Floor");
+                Floor.setImageResource(R.drawable.wh992);
+
+            }
+
+        });
+
+        Button_TF=(Button)findViewById(R.id.Button_TF);
+
+        Button_TF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title.setText("Third Floor");
+                Floor.setImageResource(R.drawable.wh993);
+
+            }
+
+        });
+
+
+
+
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
