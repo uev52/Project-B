1c56789ed7636078cb2999f1be6440bdec3a8605
Soufyan <MetaByte149@gmail.com>
2019-06-05 15:21:33 +0200

    Added adaptive background colors on every screen

--- HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/study_program_screen.java
+++ HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/study_program_screen.java
@@ -3,21 +3,15 @@ package com.example.hroopendagtest1;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.ZoomControls;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
@@ -28,6 +22,7 @@ public class study_program_screen extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         // Checks for dark theme and changes theme
         SharedPreferences preferences = getSharedPreferences(AppUtil.getPrefsName(), MODE_PRIVATE);
