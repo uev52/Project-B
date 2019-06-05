ca272ef1ab299a061b0374ed7a355c1e8a3b6cdc
Aage <aageveerman@gmail.com>
2019-06-05 05:43:25 +0200

    changes to functionality

--- HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/programming1.java
+++ HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/programming1.java
@@ -8,9 +8,16 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class programming1 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
+            EditText input1;
+            EditText input2;
+            Button   Check1;
+            View view;
 
            @Override
            protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,34 @@ public class programming1 extends AppCompatActivity implements NavigationView.On
            setContentView(R.layout.activity_programming1);
            Toolbar toolbar =  findViewById(R.id.toolbar);
            setSupportActionBar(toolbar);
+
+           input1 = findViewById(R.id.input1);
+           input2 = findViewById(R.id.input2);
+           view = this.getWindow().getDecorView();
+           Check1 = findViewById(R.id.Check1);
+           Check1.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   String firstinput = input1.getText().toString();
+                   String answer1 = "b";
+                   String secondinput = input2.getText().toString();
+                   String answer2 = "sum";
+
+                   if (firstinput.equals(answer1) && secondinput.equals(answer2)){
+                       Toast.makeText(getApplicationContext(),  "Correct!", Toast.LENGTH_LONG).show();
+                       view.setBackgroundResource(R.color.colorGreen1);
+                       return;
+                   }else{
+                       Toast.makeText(getApplicationContext(),  "Wrong!", Toast.LENGTH_LONG).show();
+                       view.setBackgroundResource(R.color.colorRed);
+                       return;
+                   }
+
+               }
+           });
+
+
+
 
 
            DrawerLayout drawer =  findViewById(R.id.drawer_layout);
@@ -29,6 +64,8 @@ public class programming1 extends AppCompatActivity implements NavigationView.On
            navigationView.setNavigationItemSelectedListener(this);
 
     }
+
+
 
     @Override
     public void onBackPressed() {
