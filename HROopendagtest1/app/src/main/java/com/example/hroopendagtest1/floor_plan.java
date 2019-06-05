d5637fd173f0ae01b37291688844398c071bdfa8
Soufyan <MetaByte149@gmail.com>
2019-06-05 16:14:59 +0200

    Darkmode'd the programming quizzes

--- HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/floor_plan.java
+++ HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/floor_plan.java
@@ -1,18 +1,16 @@
 package com.example.hroopendagtest1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class floor_plan extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,6 +21,14 @@ public class floor_plan extends AppCompatActivity implements NavigationView.OnNa
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Checks for dark theme and changes theme
+        SharedPreferences preferences = getSharedPreferences(AppUtil.getPrefsName(), MODE_PRIVATE);
+        boolean useDarkTheme = preferences.getBoolean(AppUtil.getPrefDarkTheme(),  false);
+        if(useDarkTheme) {
+            setTheme(R.style.AppTheme_Dark);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_plan);
         Toolbar toolbar =  findViewById(R.id.toolbar);
