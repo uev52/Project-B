734349d1f0b9d15bb5aada2b7f77366a987e5d2d
uev52 <45461085+uev52@users.noreply.github.com>
2019-06-10 23:38:42 +0200

    changes
    + added time to calendar event
    + made costum popup
    + limitation in sharing optings
    + added hilighted classrooms
    + changes all openday screens

--- HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/openday.java
+++ HROopendagtest1/app/src/main/java/com/example/hroopendagtest1/openday.java
@@ -23,6 +23,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,20 +38,20 @@ public class openday extends AppCompatActivity
     //set to phone calender functionality
     // in order to communicate using an API
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public void addToCalender(View view){
+    public void addToCalender(View view) {
         Intent setcalendar = new Intent(Intent.ACTION_INSERT);
         setcalendar.setType("vnd.android.cursor.item/event");
         setcalendar.putExtra(CalendarContract.Events.TITLE, "OPENDAY CMI");
         setcalendar.putExtra(CalendarContract.Events.EVENT_LOCATION, "Wijnhaven 107, 3011 WN, Rotterdam ");
         setcalendar.putExtra(CalendarContract.Events.DESCRIPTION, "Following the openday of CMI at the Hogeschool Rotterdam");
+
         // set the begin time and date of the event
         Calendar begin = Calendar.getInstance();
-        begin.set(2019,3,4, 16, 0);
+        begin.set(2019, 3, 4, 16, 0);
         // set the end time and date of the event
         Calendar end = Calendar.getInstance();
-        end.set(2019,3,4, 20, 0);
+        end.set(2019, 3, 4, 20, 0);
 
-        setcalendar.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
         //view begin time and date
         setcalendar.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
                 begin.getTimeInMillis());
@@ -58,23 +59,30 @@ public class openday extends AppCompatActivity
         setcalendar.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
                 end.getTimeInMillis());
         // start calendar activity
+
         startActivity(setcalendar);
     }
 
     //schedule and schedule classroom rows items
-    ImageView schedule,blur,floor0,floor1,floor2,floor3,floor4,floor5;
+    ImageView schedule, blur, floor0, floor1, floor2, floor3, floor4, floor5;
 
     //studie program buttons
-    Button btn_sp1, btn_sp2, btn_sp3, btn_sp4, btn_sp5;
+    Button btn_sp1;
+    Button btn_sp2;
+    Button btn_sp3;
+    Button btn_sp4;
+    Button btn_sp5;
+    ImageButton close;
 
     // openday schedule cullom and row items
-    TextView title, note,note_text,time1,time2,time3,time4,time5,time6,time7,course1,course2,course3,course4,course5,course6,course7,room1,room2,room3,room4,room5,room6,room7;
+    TextView title, note, note_text, time1, time2, time3, time4, time5, time6, time7, course1, course2, course3, course4, course5, course6, course7, room1, room2, room3, room4, room5, room6, room7;
 
     // current study program checker for day schedule
     public int sp;
 
     // Classroom information popup light box
     Dialog pop;
+    Dialog sharepop;
 
     //TextView titleTv,messageTv;
     //ImageView closePopupRoom;
@@ -88,50 +96,49 @@ public class openday extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         // create dayschedule items
-        title=(TextView) findViewById(R.id.scheduleTitle);
-        note= (TextView) findViewById(R.id.note);
-        note_text = (TextView)findViewById(R.id.note_text);
-        time1 = (TextView)findViewById(R.id.time_course1);
-        time2= (TextView)findViewById(R.id.time_course2);
-        time3= (TextView)findViewById(R.id.time_course3);
-        time4= (TextView)findViewById(R.id.time_course4);
-        time5= (TextView)findViewById(R.id.time_course5);
-        time6= (TextView)findViewById(R.id.time_course6);
+        title = (TextView) findViewById(R.id.scheduleTitle);
+        note = (TextView) findViewById(R.id.note);
+        note_text = (TextView) findViewById(R.id.note_text);
+        time1 = (TextView) findViewById(R.id.time_course1);
+        time2 = (TextView) findViewById(R.id.time_course2);
+        time3 = (TextView) findViewById(R.id.time_course3);
+        time4 = (TextView) findViewById(R.id.time_course4);
+        time5 = (TextView) findViewById(R.id.time_course5);
+        time6 = (TextView) findViewById(R.id.time_course6);
 
-        course1= (TextView)findViewById(R.id.workshop1);
-        course2= (TextView)findViewById(R.id.workshop2);
-        course3= (TextView)findViewById(R.id.workshop3);
-        course4= (TextView)findViewById(R.id.workshop4);
-        course5= (TextView)findViewById(R.id.workshop5);
-        course6= (TextView)findViewById(R.id.workshop6);
+        course1 = (TextView) findViewById(R.id.workshop1);
+        course2 = (TextView) findViewById(R.id.workshop2);
+        course3 = (TextView) findViewById(R.id.workshop3);
+        course4 = (TextView) findViewById(R.id.workshop4);
+        course5 = (TextView) findViewById(R.id.workshop5);
+        course6 = (TextView) findViewById(R.id.workshop6);
 
-        room1= (TextView)findViewById(R.id.classroom1);
-        room2= (TextView)findViewById(R.id.classroom2);
-        room3= (TextView)findViewById(R.id.classroom3);
-        room4= (TextView)findViewById(R.id.classroom4);
-        room5= (TextView)findViewById(R.id.classroom5);
-        room6= (TextView)findViewById(R.id.classroom6);
+        room1 = (TextView) findViewById(R.id.classroom1);
+        room2 = (TextView) findViewById(R.id.classroom2);
+        room3 = (TextView) findViewById(R.id.classroom3);
+        room4 = (TextView) findViewById(R.id.classroom4);
+        room5 = (TextView) findViewById(R.id.classroom5);
+        room6 = (TextView) findViewById(R.id.classroom6);
 
-        schedule = (ImageView)findViewById(R.id.schedule);
-        blur = (ImageView)findViewById(R.id.imageView4);
-        floor0 = (ImageView)findViewById(R.id.cmi1070);
-        floor1 = (ImageView)findViewById(R.id.cmi1071);
-        floor2 = (ImageView)findViewById(R.id.cmi1072);
-        floor3 = (ImageView)findViewById(R.id.cmi1073);
-        floor4 = (ImageView)findViewById(R.id.cmi1074);
-        floor5 = (ImageView)findViewById(R.id.cmi1075);
+        schedule = (ImageView) findViewById(R.id.schedule);
+        blur = (ImageView) findViewById(R.id.imageView4);
+        floor0 = (ImageView) findViewById(R.id.cmi1070);
+        floor1 = (ImageView) findViewById(R.id.cmi1071);
+        floor2 = (ImageView) findViewById(R.id.cmi1072);
+        floor3 = (ImageView) findViewById(R.id.cmi1073);
+        floor4 = (ImageView) findViewById(R.id.cmi1074);
+        floor5 = (ImageView) findViewById(R.id.cmi1075);
 
         pop = new Dialog(this);
 
 
-
         // Studyprogram button for Comunication (changes the day achedule)
-        btn_sp1=(Button)findViewById(R.id.contactBtn);
+        btn_sp1 = (Button) findViewById(R.id.contactBtn);
         sp = 0;
         btn_sp1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sp= 1;
+                sp = 1;
                 title.setText("Communicatie");
                 makeSchedule();
                 makeVisible();
@@ -140,7 +147,7 @@ public class openday extends AppCompatActivity
         });
 
         // Studyprogram button for Informatica (changes the day achedule)
-        btn_sp2=(Button)findViewById(R.id.btn_sp2);
+        btn_sp2 = (Button) findViewById(R.id.btn_sp2);
 
         btn_sp2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -154,7 +161,7 @@ public class openday extends AppCompatActivity
         });
 
         // Studyprogram button for Technisch informatica (changes the day achedule)
-        btn_sp3=(Button)findViewById(R.id.btn_sp3);
+        btn_sp3 = (Button) findViewById(R.id.btn_sp3);
 
         btn_sp3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -168,7 +175,7 @@ public class openday extends AppCompatActivity
         });
 
         // Studyprogram button for Creative Design & Game Technology (changes the day achedule)
-        btn_sp4=(Button)findViewById(R.id.btn_sp4);
+        btn_sp4 = (Button) findViewById(R.id.btn_sp4);
 
         btn_sp4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -182,7 +189,7 @@ public class openday extends AppCompatActivity
         });
 
         // Studyprogram button for Ceative Media Design (changes the day achedule)
-        btn_sp5=(Button)findViewById(R.id.btn_sp5);
+        btn_sp5 = (Button) findViewById(R.id.btn_sp5);
 
         btn_sp5.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -203,6 +210,7 @@ public class openday extends AppCompatActivity
             }
 
         });
+
 
         // classroom popup linking when clicked on text in row 2
         room2.setOnClickListener(new View.OnClickListener() {
@@ -250,7 +258,6 @@ public class openday extends AppCompatActivity
         });
 
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -271,8 +278,21 @@ public class openday extends AppCompatActivity
         }
     }
 
+    public void close1(){
+        close = (ImageButton) findViewById(R.id.close);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+
+    }
+
     // custom function makes items in dayschedule visible or invisible when needed
-    public void makeVisible(){
+    public void makeVisible() {
         blur.setVisibility(blur.INVISIBLE);
         note.setVisibility(note.INVISIBLE);
         note_text.setVisibility(note_text.INVISIBLE);
@@ -300,34 +320,117 @@ public class openday extends AppCompatActivity
     }
 
     // custom function for checking for the current chosen study program an showing popup activity when pressed on classroom
-    public void roomPlan(){
-        if (sp == 1){
+    public void roomPlan() {
+        if (sp == 1) {
             showPopupRoom();
 
         }
-        if (sp == 2){
-            showPopupRoom();
-
-
-        }
-        if (sp == 3){
-            showPopupRoom();
-
-        }
-        if (sp == 4){
+        if (sp == 2) {
             showPopupRoom();
 
 
         }
-        if (sp == 5){
+        if (sp == 3) {
+            showPopupRoom();
+
+        }
+        if (sp == 4) {
+            showPopupRoom();
+
+
+        }
+        if (sp == 5) {
             showPopupRoom();
 
         }
     }
 
     // custom function thes sets the popup class
-    public void showPopupRoom(){
-        pop.setContentView(R.layout.activity_roompop);
+    public void showPopupRoom() {
+        Intent i = new Intent(this, wijnhaven99.class);
+        startActivity(i);
+
+    }
+
+    public void showshare(View v) {
+        pop.setContentView(R.layout.share_popup);
+
+        ImageButton sharefacebook=pop.findViewById(R.id.sharefacebook);
+        sharefacebook.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                // here must come the send intent of the facebook application
+                Toast.makeText(openday.this,"an inventation is sending with facebook",Toast.LENGTH_SHORT).show();
+                Intent shareIntent1 = new Intent(Intent.ACTION_SEND);
+                shareIntent1.setType("text/plain");
+                String messageBody1 = "Hi there,\n\nThere is an openday at the Hogeschool Rotterdam on 4th of April.\nThe openday starts at 16:00 until 20:00 and takes places at Wijnhaven 107 in Rotterdam\nI will be there, would you like to join me?";
+                String messageSubject1 = "OPENDAY CMI";
+                // the sharing text/ body is set here
+                shareIntent1.putExtra(Intent.EXTRA_TEXT,messageBody1);
+                shareIntent1.putExtra(Intent.EXTRA_SUBJECT,messageSubject1);
+                shareIntent1.setPackage("com.facebook.katana");
+                startActivity(shareIntent1);
+            }
+        });
+
+        ImageButton sharewhatsapp=pop.findViewById(R.id.sharewhatsapp);
+        sharewhatsapp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                // here must come the send intent of the whatsapp application
+                Toast.makeText(openday.this,"an inventation is sending with whatsapp",Toast.LENGTH_SHORT).show();
+                Intent shareIntent1 = new Intent(Intent.ACTION_SEND);
+                shareIntent1.setType("text/plain");
+                String messageBody1 = "Hi there,\n\nThere is an openday at the Hogeschool Rotterdam on 4th of April.\nThe openday starts at 16:00 until 20:00 and takes places at Wijnhaven 107 in Rotterdam\nI will be there, would you like to join me?";
+                String messageSubject1 = "OPENDAY CMI";
+                // the sharing text/ body is set here
+                shareIntent1.putExtra(Intent.EXTRA_TEXT,messageBody1);
+                shareIntent1.putExtra(Intent.EXTRA_SUBJECT,messageSubject1);
+                shareIntent1.setPackage("com.whatsapp");
+                startActivity(shareIntent1);
+            }
+        });
+
+        ImageButton sharetwitter=pop.findViewById(R.id.sharetwitter);
+        sharetwitter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                // here must come the send intent of the twitter application
+                Toast.makeText(openday.this,"an inventation is sending with twitter",Toast.LENGTH_SHORT).show();
+                Intent shareIntent1 = new Intent(Intent.ACTION_SEND);
+                shareIntent1.setType("text/plain");
+                String messageBody1 = "Hi there,\n\nThere is an openday at the Hogeschool Rotterdam on 4th of April.\nThe openday starts at 16:00 until 20:00 and takes places at Wijnhaven 107 in Rotterdam\nI will be there, would you like to join me?";
+                String messageSubject1 = "OPENDAY CMI";
+                // the sharing text/ body is set here
+                shareIntent1.putExtra(Intent.EXTRA_TEXT,messageBody1);
+                shareIntent1.putExtra(Intent.EXTRA_SUBJECT,messageSubject1);
+                shareIntent1.setClassName("com.twitter.android", "com.twitter.android.composer.ComposerActivity");
+                startActivity(shareIntent1);
+            }
+        });
+
+        ImageButton shareemail=pop.findViewById(R.id.shareemail);
+        shareemail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                // here must come the send intent of the facebook application
+                Toast.makeText(openday.this,"an inventation is sending with email",Toast.LENGTH_SHORT).show();
+                Intent shareIntent1 = new Intent(Intent.ACTION_SEND);
+                shareIntent1.setType("text/plain");
+                String messageBody1 = "Hi there,\n\nThere is an openday at the Hogeschool Rotterdam on 4th of April.\nThe openday starts at 16:00 until 20:00 and takes places at Wijnhaven 107 in Rotterdam\nI will be there, would you like to join me?";
+                String messageSubject1 = "OPENDAY CMI";
+                // the sharing text/ body is set here
+                shareIntent1.putExtra(Intent.EXTRA_TEXT,messageBody1);
+                shareIntent1.putExtra(Intent.EXTRA_SUBJECT,messageSubject1);
+                shareIntent1.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
+                startActivity(shareIntent1);
+            }
+        });
+
 
         pop.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         pop.show();
@@ -356,7 +459,7 @@ public class openday extends AppCompatActivity
             room3.setText("H.1.308");
             room4.setText("H.1.403");
             room5.setText("H.0.310");
-            room6.setText("H.2.211");
+            room6.setText("H.2.111");
 
 
         }
@@ -370,7 +473,7 @@ public class openday extends AppCompatActivity
             room1.setText("H.0.310");
             room2.setText("H.0.310");
             room3.setText("H.0.405");
-            room4.setText("H.5.915");
+            room4.setText("H.5.314");
             room5.setText("H.0.310");
             room6.setText("H.2.403");
         }
@@ -384,7 +487,7 @@ public class openday extends AppCompatActivity
             room1.setText("H.0.310");
             room2.setText("H.0.310");
             room3.setText("H.4.308");
-            room4.setText("H.5.914");
+            room4.setText("H.5.314");
             room5.setText("H.4.206");
             room6.setText("H.4.403");
         }
@@ -400,7 +503,7 @@ public class openday extends AppCompatActivity
             room3.setText("H.3.312");
             room4.setText("H.3.206");
             room5.setText("H.1.316");
-            room6.setText("H.5.914");
+            room6.setText("H.5.314");
         }
         if (sp == 5){
             course1.setText("Intro");
@@ -411,13 +514,12 @@ public class openday extends AppCompatActivity
             course6.setText("Miniproject B");
             room1.setText("H.0.310");
             room2.setText("H.0.310");
-            room3.setText("H.2.211");
+            room3.setText("H.2.111");
             room4.setText("H.2.403");
             room5.setText("H.1.316");
             room6.setText("H.2.204");
         }
     }
-
 
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -431,34 +533,5 @@ public class openday extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-//the menu with the share button
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.openday_menu,menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
-// the sharing funtionaltiy
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()){
-// here we make an intent that gives an string/ contectext with the message
-            case R.id.share_button:
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                String messageBody = "Hi there,\n\nThere is an openday at the Hogeschool Rotterdam on 4th of April.\nThe openday starts at 16:00 until 20:00 and takes places at Wijnhaven 107 in Rotterdam\nI will be there, would you like to join me?";
-                String messageSubject = "OPENDAY CMI";
-// the sharing text/ body is set here
-                shareIntent.putExtra(Intent.EXTRA_TEXT,messageBody);
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT,messageSubject);
-// the sharing box title is set here
-                startActivity(Intent.createChooser(shareIntent,"Share with"));
-                break;
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
